df69c7cdb39b6cd0344a77f8d07884695791a8a9
Harry Patrick <harry.g.patirck94@gmail.com>
2019-12-10 10:46:46 +1100

    new array contain player move and postion of move

--- js/main1.js
+++ js/main1.js
@@ -3,16 +3,20 @@
 
 $(document).ready(function (){
 
+  let gameBoard = [
 
-    let gameBoard = [
+    ['boxOne', 'boxTwo', 'boxThree'],
+    ['boxFour', 'boxFive', 'boxSix'],
+    ['boxSeven', 'boxEight', 'boxNine']
 
-      ['boxOne', 'boxTwo', 'boxThree'],
-      ['boxFour', 'boxFive', 'boxSix'],
-      ['boxSeven', 'boxEight', 'boxNine']
+  ];
 
-    ];
+  let beenPlayed = [
 
-
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ]
 
   // saving the link to each box
   let $boxClick = $('.box');
@@ -23,30 +27,52 @@ $(document).ready(function (){
   let rowTwo = gameBoard[1];
   let rowThree = gameBoard[2];
 
+  let turnCounter = true;
+  let naughtOrCross = 'X';
+  let alreadyClicked = [];
+
+  // changes turn alternating between X and Y
+  let changeTurn = function ( count ) {
+
+    if (count === true) {
+        turnCounter = false
+        naughtOrCross = 'X'
+
+    } else if (count === false) {
+        turnCounter = true
+        naughtOrCross = 'O'
+
+    }
+
+  };
 
   let loopAppendHtml = function ( boxClicked ) {
     let toBeAppended = $(`#${boxClicked}`)//`$${boxClicked}`
     for (var i = 0; i < gameBoard.length; i++) {
       for (var j = 0; j < gameBoard[i].length; j++) {
-        if (gameBoard[i][j] === boxClicked) {
-          console.log(toBeAppended);
+        if (gameBoard[i][j] === boxClicked && beenPlayed[i][j] === '') {
+          //console.log(toBeAppended);
+          changeTurn(turnCounter);
           toBeAppended.append(naughtOrCross)
+          beenPlayed[i][j] = naughtOrCross
+
         } else {
-          console.log(toBeAppended);
+          //console.log('Choose somewhere else');
         }
       }
     }
+
   }; // add to box
 
   $boxClick.on('click', function ( ev ) {
 
-    console.log(gameBoard);
     // finds the box clicked and returns its id
     let addToBox = ev.target.id;
     // function changing returns
-    changeTurn(turnCounter);
+
     // adding naught or cross to html depending on turn
     loopAppendHtml(addToBox);
 
+    console.log(beenPlayed);
   }); // click on box
 }); // jquery document ready
