39639bea9b2d845435a1c465ec8f19984042702e
Harry Patrick <harry.g.patirck94@gmail.com>
2019-12-12 12:02:23 +1100

    first attempt split dom and logic

--- js/addToHTMLNew.js
+++ js/addToHTMLNew.js
@@ -4,16 +4,11 @@ $(document).ready(function (){
 
     gameBoard: [
 
-      ['boxOne', 'boxTwo', 'boxThree'],
-      ['boxFour', 'boxFive', 'boxSix'],
-      ['boxSeven', 'boxEight', 'boxNine']
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
 
     ],
-
-    // winCombos: [
-    //   [0, 1, 2],
-    //
-    // ],
 
     $boxClick: $('.box'),
     $flashBox: $('#flash'),
@@ -55,79 +50,31 @@ $(document).ready(function (){
 
     },
 
-    // this.gameBoard[i][j] !== ('X' || 'O')
-    // this.gameBoard[i][j] !== 'X' && this.gameBoard[i][j] !== 'O'
-
     loopAppendHtml: function ( boxClicked ) {
       let toBeAppended = $(`#${boxClicked}`)//`$${boxClicked}`
       console.log( toBeAppended );
       const x = toBeAppended.attr('x');
-      const y= toBeAppended.attr('y');
+      const y = toBeAppended.attr('y');
 
       if( toBeAppended.html() !== 'X' &&  toBeAppended.html() !== 'O' ){
         this.changeTurn(turnCounter);
         toBeAppended.append(naughtOrCross)
         this.gameBoard[x][y] = naughtOrCross;
       }
-
-      //
-      // for (var i = 0; i < this.gameBoard.length; i++) {
-      //   for (var j = 0; j < this.gameBoard[i].length; j++) {
-      //     if (this.gameBoard[i][j] === boxClicked && this.gameBoard[i][j] !== 'X' && this.gameBoard[i][j] !== 'O') {
-      //       //console.log(toBeAppended);
-      //       this.changeTurn(turnCounter);
-      //       toBeAppended.append(naughtOrCross)
-      //       this.gameBoard[i][j] = naughtOrCross;
-      //       // this.findWinnerLogic(naughtOrCross);
-      //     } else {
-      //       //console.log('Choose somewhere else');
-      //     }
-      //   }
-      // }
-
     }, //
-
-
 
     findWinnerLogic: function () {
       let checkBoard = this.gameBoard;
       for (var i = 0; i < checkBoard.length; i++) {
-            // row logic
-
             if(
                (checkBoard[i][0] === checkBoard[i][1] && checkBoard[i][2] === checkBoard[i][1] && checkBoard[i][0] !== '')
             || (checkBoard[0][i] === checkBoard[1][i] && checkBoard[2][i] === checkBoard[1][i] && checkBoard[0][i] !== '')
-            ){
-              this.gameEnd();
-              return true;
-            }
-
-          if (checkBoard[i][0] === checkBoard[i][1]
-             && checkBoard[i][2] === checkBoard[i][1]
-             && checkBoard[i][0] !== '') {
-            this.gameEnd()
-            return true
-            // colunm logic
-          } else if (checkBoard[0][i] === checkBoard[1][i]
-             && checkBoard[2][i] === checkBoard[1][i]
-             && checkBoard[0][i] !== '') {
-            this.gameEnd()
-            return true
-            // diagonal logic
-          } else if (checkBoard[0][0] === checkBoard[1][1]
-             && checkBoard[2][2] === checkBoard[1][1]
-             && checkBoard[0][0] !== '') {
-            this.gameEnd()
-            return true
-          }  else if (checkBoard[0][2] === checkBoard[1][1]
-             && checkBoard[2][0] === checkBoard[1][1]
-             && checkBoard[0][2] !== '') {
+            || (checkBoard[0][0] === checkBoard[1][1] && checkBoard[2][2] === checkBoard[1][1] && checkBoard[0][0] !== '') || (checkBoard[0][2] === checkBoard[1][1] && checkBoard[2][0] === checkBoard[1][1] && checkBoard[0][2] !== '') ) {
             this.gameEnd()
             return true
           }
-
         }
-    },
+      },
 
     addWinnerBox: function (finalMove, winningLogic) {
       if (winningLogic === true){
@@ -140,8 +87,6 @@ $(document).ready(function (){
         ['boxSeven', 'boxEight', 'boxNine']
 
       ]
-      } else {
-      console.log(finalMove);
       }
     },
 
@@ -182,11 +127,10 @@ $(document).ready(function (){
 
       console.log(ticTacToe.gameBoard);
 
-
     })
 
   }
-  ticTacToe.saveVariables()
-  ticTacToe.clickEventBox()
+    ticTacToe.saveVariables()
+    ticTacToe.clickEventBox()
 
 }); // jquery document ready
---
+++ js/split.js
@@ -0,0 +1,114 @@
+let turn = 1
+let gameOver = false
+let naughtOrCross = 'X'
+let xCounter = 0
+let oCounter = 0
+let draw = false
+
+let ticTacToe = {
+
+  gameBoard: [
+
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+
+  ],
+  // adds game winning box
+  // finds and appends box with X or Y
+  findBox: function ( boxClicked ) {
+    const playMove = $(`#${boxClicked}`)
+    const x = playMove.attr('x');
+    const y = playMove.attr('y');
+
+    if (playMove.html() !== 'X' && playMove.html() !== 'O' && gameOver === false) {
+      if (turn % 2 === 0) {
+
+        naughtOrCross = 'X'
+        this.gameBoard[x][y] = naughtOrCross
+        playMove.append('X')
+        turn++
+      } else {
+
+        naughtOrCross = 'O'
+        this.gameBoard[x][y] = naughtOrCross
+        playMove.append('O')
+        turn++
+      }
+    }
+  },
+  // finds game winner
+  findWinnerLogic: function (  ) {
+  let checkBoard = this.gameBoard;
+  for (var i = 0; i < checkBoard.length; i++) {
+        if(
+             (checkBoard[i][0] === checkBoard[i][1] && checkBoard[i][2] === checkBoard[i][1] && checkBoard[i][0] !== '')
+          || (checkBoard[0][i] === checkBoard[1][i] &&      checkBoard[2][i] === checkBoard[1][i] && checkBoard[0][i] !== '')
+          || (checkBoard[0][0] === checkBoard[1][1] && checkBoard[2][2] === checkBoard[1][1] && checkBoard[0][0] !== '')
+          || (checkBoard[0][2] === checkBoard[1][1] && checkBoard[2][0] === checkBoard[1][1] && checkBoard[0][2] !== '') ) {
+          // this.gameEnd()
+          gameOver = true
+          $('#flash').text(`Winner is player ${naughtOrCross}. play again?`)
+          $('#flash').toggle()
+      } else if (turn === 10 ) {
+        $('#flash').text(`Game is a draw... play again?`)
+        $('#flash').toggle()
+        gameOver = true
+        draw = true
+      }
+    }
+  },
+  // resets game
+  reset: function (end) {
+
+    if (end) {
+      this.gameBoard = [
+        ['', '', ''],['', '', ''],['', '', '']
+      ]
+      draw = false
+      gameOver = false
+      turn = 1
+      $('.box').empty()
+      $('#flash').toggle()
+    }
+  },
+  //  scores game
+  addScore: function ( lastMove) {
+
+    if (lastMove === 'X' && draw === false ) {
+      xCounter++
+      $('#xScore p').text(xCounter)
+    } else if (lastMove === 'O' && draw === false) {
+      oCounter++
+      $('#oScore p').text(oCounter)
+    } else {
+    }
+  },
+  // run all game functions
+  play: function ( boxClicked) {
+    this.findBox( boxClicked );
+    this.findWinnerLogic();
+
+  }
+}
+
+$(document).ready(function (  ){
+
+  // event handler play naughts and crosses
+  $('.box').on('click', function ( ev ) {
+
+    let clickPosition = ev.target.id
+    ticTacToe.play(clickPosition)
+
+  });
+
+  $('#flash').on('click', function (){
+
+
+    ticTacToe.addScore(naughtOrCross)
+    ticTacToe.reset( gameOver )
+    console.log(ticTacToe.gameBoard);
+    console.log(turn);
+
+  })
+})
